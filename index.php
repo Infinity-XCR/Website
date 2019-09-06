1900a55bd47415f5f8d829b0ef4f0261fd19a090
The-Guardians <kevenmichaud1@outlook.com>
2019-09-06 11:53:39 -0400

    Import Back-up File

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html_entity_decode>
+<html>
+<head>
+<title>Nexus Project</title>
+<img src="logo.png">
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<style>
+legend,label,footer{
+    color: red;
+}
+.footer {
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  height:5%;
+  background-color: transparent;
+ text-align: center;}
+</style>
+<strong><legend>The Real Battle Will Begin Soon</legend></strong><br>
+<form action=""method="post">
+<label>Username:<label><br>
+<input type="text"placeholder="Username"required class="form-control"><br>
+Password:<label><br>
+<input type="password"placeholder="Password" class="form-control"><br>
+Repeat-Password:<label><br>
+<input type="password"placeholder="Confirm Again" class="form-control"><br>
+Email:<label><br>
+<input type="text"placeholder="12345@hotmail.com" class="form-control"><br><br>
+<input type="submit"class="button"value="Create Account Here">
+</form>
+<form action="" method="post" class="form-1">
+          <strong><legend>Enter Your Infos Here.</legend></strong>
+			       <label>Username:</label><br>
+             <input type="text" name="username" class="form-control" required><br>  
+             <label>Password:</label><br>
+             <input type="password" name="password" class="form-control" required><br>
+             <input type="submit" class="button2" value="Login Here">
+</form>
+<div class="footer">
+<footer>
+Thank you The-Guardians we love you :)
+</footer>
+</div>
+</body>
+</html>
