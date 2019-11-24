a50dc753c65eee099244b4f43a2c95e0a4f16246
The-Guardians <kevenmichaud1@outlook.com>
2019-11-24 00:33:21 -0500

    need fix them

--- index.php
+++ index.php
@@ -2,14 +2,11 @@
 require_once('member/config.php');
 ?>
 <?php
-
 if(isset($_SESSION['username'])) {
-
    $_SESSION['msg'] = "You must log in to view this page";
    header("location: ");
 }
 if (isset($_GET['logout'])){
-
    session_destroy();
    unset($_SESSION['username']);
    header("location: login.php");
@@ -19,7 +16,7 @@ if (isset($_GET['logout'])){
 <!DOCTYPE html_entity_decode>
 <html>
 <head>
-  <title>Nexus Project</title>
+  <title>Project N.E.X.U.S</title>
   <link rel="stylesheet" type="text/css" href="style.css?__cv=<?php echo $css; ?>">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
@@ -48,7 +45,7 @@ color:red;
 </style>
 <div class="footer">
 <footer>
-<b>Designer: SNIZER | Coder: The-Guardians</b>
+<b>Designer: SNIZER | Coder: The-Guardians & Infinity</b>
 </footer>
 </div>
 </body>
